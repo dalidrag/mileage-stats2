e2188651c8136200320aa31db198049ea18f3272
Dalibor Dragojevic <daco.dragojevic@gmail.com>
2017-09-23 05:44:09 +0200

    YUIdoc documentation regenerated

--- docs/api.js
+++ docs/api.js
@@ -6,7 +6,6 @@ YUI.add("yuidoc-meta", function(Y) {
         "AddFillUpComponent",
         "AddReminderComponent",
         "AppComponent",
-        "AuthModule",
         "AuthService",
         "CarComponent",
         "CarDetailViewComponent",
@@ -36,8 +35,46 @@ YUI.add("yuidoc-meta", function(Y) {
         "UtilitiesService",
         "WellcomeComponent"
     ],
-    "modules": [],
-    "allModules": [],
+    "modules": [
+        "AddCarModule",
+        "AppModule",
+        "AuthModule",
+        "CarDetailModule",
+        "DashboardModule",
+        "RoutingModule"
+    ],
+    "allModules": [
+        {
+            "displayName": "AddCarModule",
+            "name": "AddCarModule",
+            "description": "Shows add car screen"
+        },
+        {
+            "displayName": "AppModule",
+            "name": "AppModule",
+            "description": "Main application module"
+        },
+        {
+            "displayName": "AuthModule",
+            "name": "AuthModule",
+            "description": "Contains all the authentication and user profile functionality."
+        },
+        {
+            "displayName": "CarDetailModule",
+            "name": "CarDetailModule",
+            "description": "Shows details about a car using accordion widget"
+        },
+        {
+            "displayName": "DashboardModule",
+            "name": "DashboardModule",
+            "description": "Shows dashboard components"
+        },
+        {
+            "displayName": "RoutingModule",
+            "name": "RoutingModule",
+            "description": "Contains application routing data"
+        }
+    ],
     "elements": []
 } };
 });
